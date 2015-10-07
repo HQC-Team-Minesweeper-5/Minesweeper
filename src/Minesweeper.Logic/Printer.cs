2360e9eb61fbddd3c487fd5fad82f00d3356ce3f
ykomitov <yavork@gmail.com>
2015-10-07 19:28:17 +0300

    Printer optimized

--- src/Minesweeper.Logic/Printer.cs
+++ src/Minesweeper.Logic/Printer.cs
@@ -4,8 +4,10 @@
     using System.Text;
     using Minesweeper.Logic.Enumerations;
 
-    public class Printer
+    internal class Printer
     {
+        private const string MineOpened = "* ";
+        private const string MineClosed = "? ";
         private StringBuilder sb;
 
         internal Printer()
@@ -13,85 +15,7 @@
             this.sb = new StringBuilder();
         }
 
-        public void PrintGameBoard(MineCell[,] field, int rows, int columns)
-        {
-            this.PrintTopPlayingField(rows, columns);
-
-            for (int i = 0; i < rows; i++)
-            {
-                this.sb.Append(i);
-                this.sb.Append(" | ");
-
-                for (int j = 0; j < columns; j++)
-                {
-                    MineCell currentField = field[i, j];
-
-                    if (currentField.Status == FieldStatus.Opened)
-                    {
-                        this.sb.Append(field[i, j].Value);
-                        this.sb.Append(" ");
-                    }
-                    else
-                    {
-                        this.sb.Append("? ");
-                    }
-                }
-
-                this.sb.Append("|");
-                this.sb.AppendLine();
-            }
-
-            this.sb.Append("   =");
-
-            for (int i = 0; i < columns; i++)
-            {
-                this.sb.Append("==");
-            }
-
-            this.sb.AppendLine();
-
-            Console.WriteLine(this.sb.ToString());
-            this.sb.Clear();
-        }
-
-        public void PrintAllFields(MineCell[,] field, int rows, int columns)
-        {
-            this.PrintTopPlayingField(rows, columns);
-
-            for (int i = 0; i < rows; i++)
-            {
-                this.sb.Append(i);
-                this.sb.Append(" | ");
-                for (int j = 0; j < columns; j++)
-                {
-                    MineCell currentField = field[i, j];
-                    if (currentField.Status == FieldStatus.Opened)
-                    {
-                        this.sb.Append(field[i, j].Value + " ");
-                    }
-                    else if (currentField.Status == FieldStatus.IsAMine)
-                    {
-                        this.sb.Append("* ");
-                    }
-                    else
-                    {
-                        currentField.Value = Mines.CountSurroundingMines(field, i, j);
-                        this.sb.Append(field[i, j].Value + " ");
-                    }
-                }
-
-                this.sb.Append("|");
-                this.sb.AppendLine();
-            }
-
-            this.PrintLine(columns);
-
-            this.sb.AppendLine();
-            Console.WriteLine(this.sb.ToString());
-            this.sb.Clear();
-        }
-
-        private void PrintTopPlayingField(int rows, int columns)
+        internal void PrintGameBoard(MineCell[,] field, int rows, int columns, GameStatus gamestatus)
         {
             this.sb.AppendLine();
             this.sb.Append("    ");
@@ -102,14 +26,55 @@
             }
 
             this.sb.AppendLine();
+            this.PrintLine(columns);
+
+            for (int i = 0; i < rows; i++)
+            {
+                this.sb.Append(i);
+                this.sb.Append(" | ");
+
+                for (int j = 0; j < columns; j++)
+                {
+                    MineCell currentField = field[i, j];
+
+                    if (gamestatus == GameStatus.GameOn)
+                    {
+                        if (currentField.Status == FieldStatus.Opened)
+                        {
+                            this.sb.Append(field[i, j].Value);
+                            this.sb.Append(" ");
+                        }
+                        else
+                        {
+                            this.sb.Append(MineClosed);
+                        }
+                    }
+                    else
+                    {
+                        if (currentField.Status == FieldStatus.IsAMine)
+                        {
+                            this.sb.Append(MineOpened);
+                        }
+                        else
+                        {
+                            currentField.Value = Mines.CountSurroundingMines(field, i, j);
+                            this.sb.Append(field[i, j].Value + " ");
+                        }
+                    }
+                }
+
+                this.sb.Append("|");
+                this.sb.AppendLine();
+            }
 
             this.PrintLine(columns);
+            Console.WriteLine(this.sb.ToString());
+            this.sb.Clear();
         }
 
         private void PrintLine(int columns)
         {
             this.sb.Append("   =");
-
             for (int i = 0; i < columns; i++)
             {
                 this.sb.Append("==");
