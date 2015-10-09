8462f5d7d0cf37cbc4132e70837da23e233717a9
Producenta <producenta@gmail.com>
2015-10-09 22:03:59 +0300

    Added interface IUserInput

---
+++ src/Minesweeper.Logic/Interfaces/IUserInput.cs
@@ -0,0 +1,14 @@
+﻿namespace Minesweeper.Logic.Interfaces
+{
+    public interface IUserInput
+    {
+        void HandleUserInput();
+
+        int ChoosenRow { get; set; }
+
+        int ChoosenColumn { get; set; }
+
+        // This maybe can be removed later
+        public string[] InputCoordinates { get; set; }
+    }
+}
