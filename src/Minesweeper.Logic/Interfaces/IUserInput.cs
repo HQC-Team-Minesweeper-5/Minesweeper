19c50adb6ccc6c58a14f72fd995a2d656f7f97d6
Producenta <producenta@gmail.com>
2015-10-09 22:20:35 +0300

    Added ConsoleUserInput

--- src/Minesweeper.Logic/Interfaces/IUserInput.cs
+++ src/Minesweeper.Logic/Interfaces/IUserInput.cs
@@ -2,13 +2,13 @@
 {
     public interface IUserInput
     {
-        void HandleUserInput();
-
         int ChoosenRow { get; set; }
 
         int ChoosenColumn { get; set; }
 
         // This maybe can be removed later
         public string[] InputCoordinates { get; set; }
+
+        void HandleUserInput();
     }
 }
