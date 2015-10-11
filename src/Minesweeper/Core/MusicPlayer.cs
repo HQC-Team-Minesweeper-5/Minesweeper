29eae8e909c0b1dee81cbf86ba1e20a11d65195b
ykomitov <yavork@gmail.com>
2015-10-11 23:37:34 +0300

    Documentation for MusicPlayer added - all default StyleCop rules are passed!

--- src/Minesweeper/Core/MusicPlayer.cs
+++ src/Minesweeper/Core/MusicPlayer.cs
@@ -1,10 +1,22 @@
-﻿namespace Minesweeper.Core
+﻿//-----------------------------------------------------------------------
+// <copyright file="MusicPlayer.cs" company="Team Minesweeper 5">
+//     No copyright here. You can use whatever you want!
+// </copyright>
+// <summary>This file contains the music player for our minesweeper game</summary>
+//-----------------------------------------------------------------------
+namespace Minesweeper.Core
 {
     using System;
     using System.Threading;
 
+    /// <summary>
+    /// The music player for the minesweeper game. This class is responsible for all sounds within our console game.
+    /// </summary>
     public class MusicPlayer
     {
+        /// <summary>
+        /// Plays music when player has won the game.
+        /// </summary>
         public void PlayWinMusic()
         {
             Console.Beep(600, 280);
@@ -13,12 +25,18 @@
             Console.Beep(600, 680);
         }
 
+        /// <summary>
+        /// Plays a sound when an error has been made.
+        /// </summary>
         public void PlayErrorSound()
         {
             Console.Beep(500, 280);
             Console.Beep(500, 280);
         }
 
+        /// <summary>
+        /// Plays sound when the game is over.
+        /// </summary>
         public void PlayGameOverMusic()
         {
             Console.Beep(800, 300);
@@ -26,6 +44,9 @@
             Console.Beep(400, 300);
         }
 
+        /// <summary>
+        /// Plays game music.
+        /// </summary>
         public void PlayGameMusic()
         {
             const int SoundLenght = 320;
