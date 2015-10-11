44218913f847acb1960bf82768836fe387ad452c
ykomitov <yavork@gmail.com>
2015-10-11 23:47:26 +0300

    Fixed documentation in the Memento class to pass all default StyleCop rules

--- src/Minesweeper/Core/Memento.cs
+++ src/Minesweeper/Core/Memento.cs
@@ -1,17 +1,33 @@
-﻿namespace Minesweeper.Core
+﻿//-----------------------------------------------------------------------
+// <copyright file="Memento.cs" company="Team Minesweeper 5">
+//     No copyright here. You can use whatever you want!
+// </copyright>
+// <summary>This file contains the memento logic, enabling undo functionality</summary>
+//-----------------------------------------------------------------------
+namespace Minesweeper.Core
 {
     using System.Collections.Generic;
     using System.Linq;
     using Minesweeper.Utils;
 
     /// <summary>
-    /// A static class that saves the state of the game
+    /// A static class that saves the state of the game.
     /// </summary>
     public class Memento
     {
+        /// <summary>
+        /// Field containing turn counter.
+        /// </summary>
         private int turn;
+
+        /// <summary>
+        /// List containing cell coordinates.
+        /// </summary>
         private List<List<Coordinates>> cells;
 
+        /// <summary>
+        /// Initializes a new instance of the Memento class.
+        /// </summary>
         public Memento()
         {
             this.turn = -1;
@@ -19,9 +35,9 @@
         }
 
         /// <summary>
-        /// A method that adds the cells that are changed this turn to the list
-        /// <param name="newCells">The cells that are changed this turn</param>
+        /// A method that adds the cells that are changed this turn to the list.
         /// </summary>
+        /// <param name="newCells">The cells that are changed this turn.</param>
         public void AddCells(List<Coordinates> newCells)
         {
             this.cells.Add(newCells);
@@ -30,7 +46,7 @@
         }
 
         /// <summary>
-        /// A method that removes the cells that are changed the last turn from the list
+        /// A method that removes the cells that are changed the last turn from the list.
         /// </summary>
         public void RemoveCells()
         {
@@ -42,8 +58,9 @@
         }
 
         /// <summary>
-        /// A method that returns the cells that are changed the last turn from the list
+        /// A method that returns the cells that are changed the last turn from the list.
         /// </summary>
+        /// <returns>The cells that were changed in the last turn.</returns>
         public List<Coordinates> GetLastCells()
         {
             if (this.turn >= 0)
