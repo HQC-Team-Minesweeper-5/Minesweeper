49ddfd65c7ab0b1bf16c86d49b5c94b3a72e3fe1
ykomitov <yavork@gmail.com>
2015-10-11 23:50:56 +0300

    Fixed GameStatus documentation to pass all default StyleCop rules.

--- src/Minesweeper/Core/GameStatus.cs
+++ src/Minesweeper/Core/GameStatus.cs
@@ -1,10 +1,34 @@
-﻿namespace Minesweeper.Core
+﻿//-----------------------------------------------------------------------
+// <copyright file="GameStatus.cs" company="Team Minesweeper 5">
+//     No copyright here. You can use whatever you want!
+// </copyright>
+// <summary>This enumeration lists the different states of the minesweeper game.</summary>
+//-----------------------------------------------------------------------
+namespace Minesweeper.Core
 {
+    /// <summary>
+    /// Enumeration with the different states of the minesweeper game.
+    /// </summary>
     public enum GameStatus
     {
+        /// <summary>
+        /// State where the game is currently being played.
+        /// </summary>
         GameOn,
+
+        /// <summary>
+        /// State where player has won the game.
+        /// </summary>
         YouWin,
+
+        /// <summary>
+        /// State where the game will be restarted.
+        /// </summary>
         Restart,
+
+        /// <summary>
+        /// State where the game is over.
+        /// </summary>
         GameOver
     }
 }
