917032abc0ca2f8ef6d49f511a9e1e90c22803dc
Steven Tsvetkov <steven.tsvetkov@gmail.com>
2015-10-12 13:36:27 +0300

    Update LevelTests.cs

--- tests/Minesweeper.Tests/LevelTests.cs
+++ tests/Minesweeper.Tests/LevelTests.cs
@@ -7,6 +7,38 @@
     [TestClass]
     public class LevelTests
     {
+        private readonly int numberOfRows = 5;
+        private readonly int numberOfCols = 5;
+        private readonly int numberOfMines = 5;
+
+        [TestMethod]
+        public void InitializingLevelShouldNotThrow()
+        {
+            var level = new Level(this.numberOfRows, this.numberOfCols, this.numberOfMines);
+        }
+
+        [TestMethod]
+        public void NumberOfRowsShouldBeInt()
+        {
+            var level = new Level(this.numberOfRows, this.numberOfCols, this.numberOfMines);
+            Assert.IsInstanceOfType(level.NumberOfRows, typeof(int));
+        }
+
+        [TestMethod]
+        public void NumberOfColsShouldBeInt()
+        {
+            var level = new Level(this.numberOfRows, this.numberOfCols, this.numberOfMines);
+            Assert.IsInstanceOfType(level.NumberOfCols, typeof(int));
+        }
+
+        [TestMethod]
+        public void NumberOfMinesShouldBeInt()
+        {
+            var level = new Level(this.numberOfRows, this.numberOfCols, this.numberOfMines);
+            Assert.IsInstanceOfType(level.NumberOfMines, typeof(int));
+        }
+    }
+        
         [TestMethod]
         public void CreateLevelShouldCreateNewLevel()
         {
