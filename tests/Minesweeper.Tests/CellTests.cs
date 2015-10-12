7af29d25641c150cfb6bc1683cf59d69e887b4b6
Steven Tsvetkov <steven.tsvetkov@gmail.com>
2015-10-12 15:37:18 +0300

    Adding CellTests

---
+++ tests/Minesweeper.Tests/CellTests.cs
@@ -0,0 +1,46 @@
+﻿using Minesweeper.Core.Field;
+
+namespace Minesweeper.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Minesweeper.Core.Mines;
+
+    [TestClass]
+    public class CellTests
+    {
+        [TestMethod]
+        public void InitializingNewCellShouldNotThrow()
+        {
+            var cell = new Cell();
+        }
+
+        [TestMethod]
+        public void ValueShouldReturnInt()
+        {
+            var cell = new Cell();
+            Assert.IsInstanceOfType(cell.Value, typeof(int));
+        }
+
+        [TestMethod]
+        public void StatusShouldReturnCellStatus()
+        {
+            var cell = new Cell();
+            Assert.IsInstanceOfType(cell.Status, typeof(CellStatus));
+        }
+
+        [TestMethod]
+        public void IsMineShouldReturnBool()
+        {
+            var cell = new Cell();
+            Assert.IsInstanceOfType(cell.IsMine, typeof(bool));
+        }
+
+        [TestMethod]
+        public void CloneShouldReturnObject()
+        {
+            var cell = new Cell();
+            Assert.IsInstanceOfType(cell.Clone(), typeof(Cell));
+        }
+    }
+}
