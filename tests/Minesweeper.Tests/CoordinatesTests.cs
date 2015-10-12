ad2454d44849291634a4b4de07b9c6a2ec9e9af4
Steven Tsvetkov <steven.tsvetkov@gmail.com>
2015-10-12 14:16:24 +0300

    Adding coordinates tests

---
+++ tests/Minesweeper.Tests/CoordinatesTests.cs
@@ -0,0 +1,31 @@
+﻿namespace Minesweeper.Tests.UitlsTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Minesweeper.Utils;
+
+    [TestClass]
+    public class CoordinatesTests
+    {
+        private readonly int row = 5;
+        private readonly int column = 5;
+
+        [TestMethod]
+        public void InitializingCoordinatesShouldNowThrow()
+        {
+            var coordinate = new Coordinates(this.row, this.column);
+        }
+
+        public void RowShouldBeInt()
+        {
+            var coordinate = new Coordinates(this.row, this.column);
+            Assert.IsInstanceOfType(coordinate.Row, typeof(int));
+        }
+
+        public void ColShouldBeInt()
+        {
+            var coordinate = new Coordinates(this.row, this.column);
+            Assert.IsInstanceOfType(coordinate.Col, typeof(int));
+        }
+    }
+}
